8e61ea8bf55e72791628df8d8fb0871ea28bb4f4
Ora-bot-trading <ingenierosfinancieros5@gmail.com>
2025-06-06 23:02:25 -0500

    Update __init__.py

--- data_collection/__init__.py
+++ data_collection/__init__.py
@@ -2,3 +2,4 @@ from .market_data import MarketDataCollector
 from .social_media import SocialMediaCollector
 from .on_chain import OnChainCollector
 from .exchange import ExchangeCollector
+soy jose
